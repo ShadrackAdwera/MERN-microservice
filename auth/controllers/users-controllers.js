f73cf659559f3289431e30980a457ece1a254a96
ShadrackAdwera <adweshshaddie@gmail.com>
2021-09-24 12:36:31 +0300

    create sign in controller

--- auth/controllers/users-controllers.js
+++ auth/controllers/users-controllers.js
@@ -10,7 +10,7 @@ const currentUser = {
     name: 'Deez Nuts',
     email: 'deeznuts@mail.com'
 }
-const users = [];
+const USERS = [];
 
 const getCurrentUser = (req,res,next) => {
     res.status(200).json({users: currentUser})
@@ -26,7 +26,7 @@ const signUp = async(req,res,next) => {
     let hashedPassword;
     let token;
     try {
-        foundEmail = users.find(user=>user.email===email);
+        foundEmail = USERS.find(user=>user.email===email);
     } catch (error) {
         return next(new HttpError('Internal server error',500));
     }
@@ -42,7 +42,7 @@ const signUp = async(req,res,next) => {
     
     const createdUser = {id: crypto.randomUUID().toString(), email, password: hashedPassword}
 
-    users.push(createdUser);
+    USERS.push(createdUser);
     try {
         token = jwt.sign({id: createdUser.id, email: createdUser.email}, 'supersecretkey', { expiresIn: '1h' });
     } catch (error) {
@@ -55,13 +55,35 @@ const signIn = (req,res,next) => {
     const { email, password } = req.body;
     const error = validationResult(req);
 
+    if(!error.isEmpty()) {
+        return next(new HttpError('Invalid email or password',422));
+    }
     //check if email exists in db
+    const foundUser = USERS.find(user=>user.email===email);
+    let isPassword;
+    let token;
+    if(!foundUser) {
+        return next(new HttpError('Email does not exist, try signing up first', 422));
+    }
+
     //check if password for email is correct
+    try {
+        isPassword = await bcrypt.compare(password, foundUser.password);
+    } catch (error) {
+        return next(new HttpError('An error occured, try again', 500));
+    }
+    if(!isPassword) {
+        return next(new HttpError('Auth failed', 401));
+    }
     //create auth token
-    //create user
-    users.push({email, password});
-    res.status(201).json({message: 'Sign up successful', user: { email }})
+    try {
+        token = jwt.sign({id: foundUser.id, email}, 'supersecretkey', {expiresIn: '1h'})
+    } catch (error) {
+        return next(new HttpError('Auth failed', 401));
+    }
+    res.status(201).json({message: 'Sign up successful', user: { id: foundUser.id, email, token }})
 }
 
 exports.getCurrentUser = getCurrentUser;
 exports.signUp = signUp;
+exports.signIn = signIn;
