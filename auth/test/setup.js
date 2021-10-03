adfd992f6c3dd71b75cb7d3172bfb87c13579c22
ShadrackAdwera <adweshshaddie@gmail.com>
2021-10-03 06:51:35 +0300

    configure before and after test hooks

--- auth/test/setup.js
+++ auth/test/setup.js
@@ -2,3 +2,26 @@ const { MongoMemoryServer } = require('mongodb-memory-server');
 const mongoose = require('mongoose');
 const app = require('../index');
 
+let mongo;
+beforeAll(async()=>{
+    mongo = new MongoMemoryServer();
+    const mongoUri = await mongo.getUri();
+
+    try {
+        await mongoose.connect(mongoUri, ()=>{
+            console.log('Connected to Mongo Memory Server');
+        });
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+beforeEach(async()=>{
+    const collections = await mongoose.connection.db.collections();
+    collections.forEach(coll=>await coll.deleteMany({}))
+})
+
+afterAll(async()=>{
+    await mongo.stop();
+    await mongoose.connection.close();
+})
