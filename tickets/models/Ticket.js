ff90bea32329e179bf17693dc51a40106a794fec
ShadrackAdwera <adweshshaddie@gmail.com>
2021-10-11 11:46:07 +0300

    create ticket model

---
+++ tickets/models/Ticket.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const ticketSchema = new Schema({
+    title: { type: String, required: true },
+    price: {type: String, required: true },
+    user: { type: Schema.Types.ObjectId, required: true, ref: 'User' }
+}, { timestamps: true })
+
+module.exports = mongoose.model('Ticket', ticketSchema);
