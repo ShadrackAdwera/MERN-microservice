33e0ea6f89b4b2a8fa0d75f8e8752b140b6cdb16
ShadrackAdwera <adweshshaddie@gmail.com>
2021-10-11 13:21:39 +0300

    create mongodb service for tickets and update connection string url

--- tickets/app.js
+++ tickets/app.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const app = require('./index');
 
-mongoose.connect('').then(()=>{
+mongoose.connect('mongodb://tickets-mongo-service:27017/tickets').then(()=>{
     app.listen(5001);
     console.log('Connected to tickets DB, listenting on port: 5001');
 }).catch(error=>console.log(error));
