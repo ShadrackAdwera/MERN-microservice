6c769d74b10958fe83f6bb4b03b526607b3d4b1c
ShadrackAdwera <adweshshaddie@gmail.com>
2021-10-11 12:00:21 +0300

    create get ticket by user controller

--- tickets/controllers/ticket-controller.js
+++ tickets/controllers/ticket-controller.js
@@ -1,13 +1,27 @@
 const { HttpError } = require('@adwesh/common/src/index');
 const Ticket = require('../models/Ticket');
 
+const populateQuery = [
+    { path: 'user', select: ['email','_id'] }
+]
+
 const getTickets = async(req,res,next) => {
     let tickets;
     try {
-        tickets = await Ticket.find().exec();
+        tickets = await Ticket.find().populate(populateQuery).exec();
     } catch (error) {
         return next(new HttpError('Unable to fetch tickets', 500));
     }
     res.status(200).json({tickets: tickets.map(ticket=>ticket.toObject({getters: true}))})
 }
 
+const getUserTickets = async(req,res,next) => {
+    const { userId } = req.params;
+    let foundTickets
+    try {
+        foundTickets = await Ticket.find({user: userId}).populate(populateQuery).exec();
+    } catch (error) {
+        return next(new HttpError('Unable to fetch tickets', 500));
+    }
+    res.status(200).json({tickets: foundTickets.map(foundTicket=>foundTicket.toObject({getters: true}))})
+}
