c74e8a510cd05956d923966297582bc75a97c225
ShadrackAdwera <adweshshaddie@gmail.com>
2021-10-11 12:27:23 +0300

    update ticket controller

--- tickets/controllers/ticket-controller.js
+++ tickets/controllers/ticket-controller.js
@@ -10,12 +10,13 @@ const addTickets = async(req,res,next) => {
     const { userId } = req.user;
     const { title, price } = req.body
     let createdTicket;
-    let foundUser;
+    //let foundUser;
 
-    //check if user exists*
+    //TO DO: check if user exists*
 
     try {
         createdTicket = new Ticket({title, price, user: userId});
+        await createdTicket.save();
     } catch (error) {
         return next(new HttpError('Unable to add ticket', 500));
     }
@@ -42,4 +43,34 @@ const getUserTickets = async(req,res,next) => {
         return next(new HttpError('Unable to fetch tickets', 500));
     }
     res.status(200).json({tickets: foundTickets.map(foundTicket=>foundTicket.toObject({getters: true}))})
+}
+
+//UPDATE
+const updateTicket = async(req,res,next) => {
+    const { userId } = req.user;
+    const { title } = req.body;
+    const { ticketId } = req.params;
+    let foundTicket;
+
+    //TODO: Check if user exists*
+
+    try {
+     foundTicket = await Ticket.findById(ticketId).exec();
+    } catch (error) {
+        return next(new HttpError('Unable to fetch ticket', 500));
+    }
+    if(!foundTicket) {
+        return next(new HttpError('This ticket does not exist', 404));
+    }
+    if(foundTicket._id.toString() !== userId.toString()) {
+        return next(new HttpError('You are not authorized to perform this action', 403));
+    }
+    foundTicket.title = title;
+    try {
+        await foundTicket.save()
+    } catch (error) {
+        return next(new HttpError('An error occured while updating the ticket', 500));
+    }
+    res.status(200).json({message: 'Ticket information successfully updated', ticket: foundTicket.toObject({getters: true})})
+
 }
