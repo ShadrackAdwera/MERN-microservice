ed37d789d5e82e633c2ac3373667f176e5231cf7
ShadrackAdwera <adweshshaddie@gmail.com>
2021-10-21 12:43:57 +0300

    add validation to check if ticket is reserved

--- orders/controller/order-controller.js
+++ orders/controller/order-controller.js
@@ -1,15 +1,15 @@
 const { validationResult } = require("express-validator");
-const nats = require('node-nats-streaming');
-const HttpError = require('@adwesh/common/src/error/httpError');
+const nats = require("node-nats-streaming");
+const HttpError = require("@adwesh/common/src/error/httpError");
 const Publisher = require("@adwesh/common/src/events/base-publisher");
 
-const Order = require('../model/order-model');
-const Ticket = require('../model/ticket-model');
+const Order = require("../model/order-model");
+const Ticket = require("../model/ticket-model");
 
-Date.prototype.addHours = function(h){
-    this.setHours(this.getHours()+h);
-    return this;
-}
+Date.prototype.addHours = function (h) {
+  this.setHours(this.getHours() + h);
+  return this;
+};
 
 const populateQuery = [{ path: "ticket", select: ["title", "price"] }];
 
@@ -25,36 +25,55 @@ awaiting:payment - order successfully reserved the ticket.
 complete - order has reserved the ticket and user has paid the amount successfully.
 */
 
-const createOrder = async(req,res,next) => {
-    const error = validationResult(req);
-    if(!error.isEmpty()) {
-        return next(new HttpError('Invalid inputs', 422));
-    }
-    let foundTicket;
-    const { userId } = req.user;
-    //check if user exists
-    const { ticketId } = req.body;
-    //check if ticket exists
-    try {
-        foundTicket = await Ticket.findById(ticketId).populate().exec();
-    } catch (error) {
-        return next(new HttpError('An error occured, try again', 500));
-    }
-    if(!foundTicket) {
-        return next(new HttpError('This ticket does not exist', 404));
-    }
+const createOrder = async (req, res, next) => {
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return next(new HttpError("Invalid inputs", 422));
+  }
+  let foundTicket;
+  let foundOrder;
+  const { userId } = req.user;
+  //TODO: check if user exists
+  const { ticketId } = req.body;
+  //check if ticket exists
+  try {
+    foundTicket = await Ticket.findById(ticketId).populate().exec();
+  } catch (error) {
+    return next(new HttpError("An error occured, try again", 500));
+  }
+  if (!foundTicket) {
+    return next(new HttpError("This ticket does not exist", 404));
+  }
 
-    const createdOrder = new Order({
-        userId, status: 'pending', expiresAt: new Date().addHours(1), ticket: ticketId
-    })
+  //check if ticket is not reserved
+  try {
+    foundOrder = await foundTicket.isReserved()
+  } catch (error) {
+      return next(new HttpError('Unable to fetch orders', 500));
+  }
+  if(foundOrder) {
+      return next(new HttpError('This ticket has already been reserved', 422))
+  }
 
-    try {
-        await createdOrder.save();
-    } catch (error) {
-        return next(new HttpError('Unable to save order', 500));
-    }
-    res.status(201).json({message: 'Your order was reserved!', order: createdOrder})
-}
+  const createdOrder = new Order({
+    userId,
+    status: "pending",
+    expiresAt: new Date().addHours(1),
+    ticket: ticketId,
+  });
+
+  try {
+    await createdOrder.save();
+  } catch (error) {
+    return next(new HttpError("Unable to save order", 500));
+  }
+
+  //TODO: Publish an event for an event being created
+
+  res
+    .status(201)
+    .json({ message: "Your order was reserved!", order: createdOrder });
+};
 
 //get orders
 // const getOrders = async(req,res,next) => {
@@ -67,52 +86,57 @@ const createOrder = async(req,res,next) => {
 //     res.status(200).json({orders: foundOrders.map(ord=>ord.toObject({getters: true}))})
 // }
 
-const getOrderById = async(req,res,next) => {
-    let foundOrder;
-    const { id } = req.params;
-    try {
-        foundOrder = await Order.findById(id).populate(populateQuery);
-    } catch (error) {
-        return next(new HttpError('Internal server error',500));
-    }
-    if(!foundOrder) {
-        return next(new HttpError('This order does not exists', 404));
-    }
-    res.status(200).json({order: foundOrder.toObject({getters: true})});
-}
+const getOrderById = async (req, res, next) => {
+  let foundOrder;
+  const { id } = req.params;
+  try {
+    foundOrder = await Order.findById(id).populate(populateQuery);
+  } catch (error) {
+    return next(new HttpError("Internal server error", 500));
+  }
+  if (!foundOrder) {
+    return next(new HttpError("This order does not exists", 404));
+  }
+  res.status(200).json({ order: foundOrder.toObject({ getters: true }) });
+};
 
-const getUserOrders = async(req,res,next) => {
-    const { userId } = req.user;
-    let foundOrders;
+const getUserOrders = async (req, res, next) => {
+  const { userId } = req.user;
+  let foundOrders;
 
-    try {
-        foundOrders = await Order.find({userId}).populate(populateQuery).exec();
-    } catch (error) {
-        return next(new HttpError('Failed to fetch orders', 500));
-    }
-    res.status(200).json({orders: foundOrders.map(ord=>ord.toObject({getters: true}))})
+  try {
+    foundOrders = await Order.find({ userId }).populate(populateQuery).exec();
+  } catch (error) {
+    return next(new HttpError("Failed to fetch orders", 500));
+  }
+  res
+    .status(200)
+    .json({
+      orders: foundOrders.map((ord) => ord.toObject({ getters: true })),
+    });
+};
+const deleteOrders = async (req, res, next) => {
+  const { id } = req.params;
+  let foundOrder;
 
-}
-const deleteOrders = async(req,res,next) => {
-    const { id } = req.params
-    let foundOrder;
+  try {
+    foundOrder = await Order.findById(id).exec();
+  } catch (error) {
+    return next(new HttpError("Unable to fetch order", 500));
+  }
+  if (!foundOrder) {
+    return next(new HttpError("This order does not exist", 404));
+  }
 
-    try {
-        foundOrder = await Order.findById(id).exec();
-    } catch (error) {
-        return next(new HttpError('Unable to fetch order',500));
-    }
-    if(!foundOrder) {
-        return next(new HttpError('This order does not exist', 404));
-    }
-
-    try {
-        await Order.findByIdAndDelete(id).exec();
-    } catch (error) {
-        return next(new HttpError('An error occured while deleting the order, try again', 500));
-    }
-    res.status(200).json({message: 'This order was deleted successfully'})
-}
+  try {
+    await Order.findByIdAndDelete(id).exec();
+  } catch (error) {
+    return next(
+      new HttpError("An error occured while deleting the order, try again", 500)
+    );
+  }
+  res.status(200).json({ message: "This order was deleted successfully" });
+};
 
 //exports.getOrders = getOrders;
 exports.getOrderById = getOrderById;
--- orders/model/ticket-model.js
+++ orders/model/ticket-model.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const Order = require('./order-model');
 
 const ticketSchema = new Schema({
     title: { type: String, required: true },
-    price: { type: Number, required: true },
+    price: { type: Number, required: true, min: 0 },
     version: { type: Number, required: true }
 });
+
+ticketSchema.methods.isReserved = async function() {
+    const foundOrder = await Order.findOne({
+        ticket: this,
+        status: { $in: ["created", "awaiting:payment", "complete"] },
+      });
+      return !!foundOrder
+}
 
 module.exports = mongoose.model('Ticket', ticketSchema);
