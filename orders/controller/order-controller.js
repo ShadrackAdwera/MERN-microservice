3afb55d67acb47ca9936a5d0044463f305c7dc27
ShadrackAdwera <adweshshaddie@gmail.com>
2021-10-21 07:20:02 +0300

    get orders by id controller

--- orders/controller/order-controller.js
+++ orders/controller/order-controller.js
@@ -26,3 +26,18 @@ const getOrders = async(req,res,next) => {
     }
     res.status(200).json({orders: foundOrders.map(ord=>ord.toObject({getters: true}))})
 }
+
+const getOrderById = async(req,res,next) => {
+    let foundOrder;
+    const { id } = req.params;
+    try {
+        foundOrder = await Order.findById(id);
+    } catch (error) {
+        return next(new HttpError('Internal server error',500));
+    }
+    if(!foundOrder) {
+        return next(new HttpError('This order does not exists', 404));
+    }
+    res.status(200).json({order: foundOrder.toObject({getters: true})});
+}
+
